82913c746e9c40978d35c977f46196a9302efd2c
Pinxiang Wang <pwang25@ncsu.edu>
2022-07-31 15:19:40 -0400

    111

---
+++ src/leetcode127_WordLadder.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class leetcode127_WordLadder {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<String> queue = new LinkedList<>();
+        Set<String> words = new HashSet<>();
+        words.remove(beginWord);
+        queue.add(beginWord);
+        int level = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            level++;
+            for (int i = 0; i < size; i++) {
+                String currentWord = queue.poll();
+                if(currentWord.equals(endWord)){
+                    return level;
+                }
+                List<String> neighbors = neighbors(currentWord);
+                for(String neigh:neighbors){
+                    if(words.contains(neigh)) {
+                        words.remove(neigh);
+                        queue.add(neigh);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+    public List<String> neighbors(String currentWord){
+        
+    }
+}
